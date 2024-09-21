f70505143e171ff20b89e3ca2784f4faba623f1d
Jacob Mayer <jmayereup@gmail.com>
2024-09-21 19:20:13 +0700

    removed content scripts

--- background.js
+++ background.js
@@ -1,3 +1,35 @@
-chrome.action.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, { action: "sendSelectedText" });
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: sendSelectedTextToPPR
+  })
+});
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "readOnPPR",
+    title: "Read on PPR",
+    contexts: ["selection"]
   });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "readOnPPR") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: sendSelectedTextToPPR
+    });
+  }
+});
+
+function sendSelectedTextToPPR() {
+  const selectedText = window.getSelection().toString();
+  if (selectedText.trim() === "") {
+    alert("Please select some text before using the context menu.");
+  } else {
+    const encodedText = encodeURIComponent(selectedText);
+    const targetUrl = `https://purplepeoplesreader.com/list/USER/A1?text=${encodedText}`;
+    window.open(targetUrl, "_blank");
+  }
+}
